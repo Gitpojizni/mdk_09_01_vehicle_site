01e43df3e0e46930b0710dfed1bb934f13e4543d
Gitpojizni <69abobu228@gmail.com>
2025-10-17 17:17:10 +0900

    17_10_25
    comment

--- FilipskyVehicleSite/tovar.php
+++ FilipskyVehicleSite/tovar.php
@@ -40,12 +40,11 @@
 		</div>
 		</header>
 		<main>
-			<div class="main_panel">
+			<div class="main_panel_tovar">
 				<? 
 				$id=$_POST['id'];
 				$query="SELECT * FROM `tovar` WHERE `id` = $id ";
 				$result=mysqli_query($conn, $query);
-				$row=mysqli_fetch_array($result);
 
 				?>
 				<center>
@@ -58,6 +57,49 @@
 						<p class='fix_dla_p'><? echo $row[0]; ?></p>
 					</div>
 				</center>
+				<div class="comment_section">
+					<div class="comment_view_scrollbar">
+						<?
+						$comment=$_POST['comment'];
+						$comment_text = $_POST['comment_text'];
+						if ($_POST['send'] == 'Отправить') {
+							$query="INSERT INTO comment (`user_id`, `login`, `status`, `comment`)
+									VALUES ('228', 'logos', '227', '$comment_text')";
+						}
+						$query="SELECT * FROM comment";
+						$result=mysqli_query($conn, $query);
+						$row=mysqli_fetch_array($result);
+						
+						?>
+						
+						<? echo count($row); ?>
+						<? echo count($row[3]); 
+												print_r($row)
+
+						?>
+						
+						<?
+						for ($i = 1; $i <= count($row); $i++)
+						{
+						?>
+						<div class="comment_block">
+							<p> <? echo $row[1]; ?></p>
+							<p><? echo $row[2]; ?></p>
+							<p><? echo $row[3]; ?></p>
+							<p><? echo $row[4]; ?></p>
+							
+						</div>
+						<? 
+						}
+						?>	
+					</div>
+					<div class="comment_redaction_section">
+						<form id="comment_redact_and_send" method="post" action="tovar.php">
+							<input id="comment_redact" type="text" name="comment_text">
+							<input id="comment_send" type="submit" name="send" value="Отправить">
+						</form>
+					</div>
+				</div>
 			</div>
 		</main>
 		<footer>
